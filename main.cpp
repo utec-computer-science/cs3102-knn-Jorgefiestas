297c007161b3f6fb76d6f445fe2b39ea72d9c676
Jorge Ignacio Fiestas Lavezzolo <jorge.fiestas@utec.edu.pe>
2020-04-12 21:01:02 -0500

    refactor(): Removes std from main fuction

--- main.cpp
+++ main.cpp
@@ -8,7 +8,6 @@
 #include <chrono> 
 #include <stdio.h>
 #include <unistd.h>
-using namespace std;
 
 template <typename T>
 class CartesianCoord {
@@ -24,7 +23,7 @@ class CartesianCoord {
 		}
 
 		template<typename _T>
-		friend ostream& operator<<(ostream &out, const CartesianCoord<_T> c){
+		friend std::ostream& operator <<(std::ostream &out, const CartesianCoord<_T> c){
 			out << "(" << c.x << "," << c.y << ")";
 			return out;
 		}
@@ -33,7 +32,6 @@ class CartesianCoord {
 
 typedef CartesianCoord<int> coord_t;
 
-
 float euclideanDistance(coord_t p1, coord_t p2) {
 	float dx = (float) (p1.x - p2.x);
 	float dy = (float) (p1.y - p2.y);
@@ -41,47 +39,53 @@ float euclideanDistance(coord_t p1, coord_t p2) {
 	return sqrt(dx * dx + dy * dy);
 }
 
-
-std::vector<coord_t> knn (int k, vector<coord_t> &points, const coord_t &q) {
+std::vector<coord_t> knn (int k, const std::vector<coord_t> &points, const coord_t &q) {
 	std::vector<std::pair<float, size_t>> distances;
 
 	for (size_t i = 0; i < points.size(); i++) {
 		coord_t coor = points[i];
 		float dist = euclideanDistance(coor, q);
-
-		distances.push_back({dist, i});
+		std::pair<float, size_t> distPoint = {dist, i};
+		distances.emplace_back(distPoint);
 	}
 
-	std::nth_element(distances.begin(), distances.begin() + k - 1, distances.end());
+	std::nth_element(
+			distances.begin(), 
+			distances.begin() + k - 1, 
+			distances.end()
+			);
 	
-	std::vector<coord_t> nn(k);
+	std::vector<coord_t> nns(k);
 	for (size_t i = 0; i < k; i++) {
-		nn[i] = points[distances[i].second];
+		nns[i] = points[distances[i].second];
 	}
 	
-	return nn;
+	return nns;
 }
 
 
 int main() {
 	srand (time(NULL));
 	
-	vector<coord_t> points;
-	for(int i=0; i< 1000000; i++)
-		points.push_back(coord_t(rand()%1000,rand()%1000));
+	std::vector<coord_t> points;
+	for (int i = 0; i < 10000000; i++) {
+		points.push_back(coord_t(rand() % 1000, rand() % 1000));
+	}
 		
-	vector<coord_t> knns;
-	auto start = chrono::high_resolution_clock::now(); 
-	knns = knn(3, points, coord_t(100,200));
-	auto stop = chrono::high_resolution_clock::now(); 
-	auto duration = chrono::duration_cast<chrono::seconds>(stop - start);
-	cout << "Time: " << endl;
-	cout << duration.count() << endl;
+	std::vector<coord_t> knns;
 
-	cout << "knns" << endl;
-	vector<coord_t>::iterator kit = knns.begin();
-	for (; kit != knns.end(); kit++)
-		cout << *kit << endl; 
+	auto start = std::chrono::high_resolution_clock::now(); 
+	knns = knn(3, points, coord_t(100, 200));
+	auto stop = std::chrono::high_resolution_clock::now(); 
 
+	auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
 
+	std::cout << "Time: " << std::endl;
+	std::cout << duration.count() << std::endl;
+	std::cout << "knns" << std::endl;
+	for (auto kit = knns.begin(); kit != knns.end(); kit++) {
+		std::cout << *kit << std::endl; 
+	}
+
+	return 0;
 }
